5b7f87d0c721f8ef6ac15e2e9ca62715f36d286a
vallecbg <valioplayofficial@gmail.com>
2020-05-29 00:36:03 +0300

    Created Cube Model and Storage of Cubes inside a JSON File as a Database

--- JS Back-End/Cubicle/index.js
+++ JS Back-End/Cubicle/index.js
@@ -1,9 +1,20 @@
 const env = process.env.NODE_ENV || 'development';
+global.__basedir = __dirname;
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const cubeModel = require('./models/cube');
 
-require('./config/express')(app);
-require('./config/routes')(app);
+cubeModel.insert({name: 'test1', desc: 'test1'}).then(insertedCube => {
+    console.log(insertedCube);
+    return cubeModel.delete(insertedCube.id);
+}).then((deletedCube) => {
+    console.log(deletedCube);
+    console.log("successfully");
+});
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+// const config = require('./config/config')[env];
+// const app = require('express')();
+
+// require('./config/express')(app);
+// require('./config/routes')(app);
+
+// app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
